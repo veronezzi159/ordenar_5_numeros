00c0ff5c6c6e3d0f29c1d12c5c14661f58875d23
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-23 14:34:23 -0300

    bubble sort

--- ordenar_5_numeros/Program.cs
+++ ordenar_5_numeros/Program.cs
@@ -1,17 +1,13 @@
 ﻿int[] numeros = new int[5], ordenado = new int[5];
 int aux = 0, menor = 0, j = 0, maior = 0;
 
-Console.WriteLine("Escreva 5 números inteiros positivos");
+Console.WriteLine("");
 for (int i = 0; i < 5; i++)
 {
-    Console.WriteLine($"Escreva o {i+1}º número");
-    numeros[i] = int.Parse(Console.ReadLine());
-}
-
-for (int i = 0; i < 5; i++)
-{
+    numeros[i] = new Random().Next(0,100);
     ordenado[i] = numeros[i];
 }
+
 
 for(int i = 0; i < 5; i++)
 {
@@ -22,6 +18,7 @@ for(int i = 0; i < 5; i++)
             aux = ordenado[j];
             ordenado[j] = ordenado[i];
             ordenado[i] = aux;
+
         }
     }
 }
