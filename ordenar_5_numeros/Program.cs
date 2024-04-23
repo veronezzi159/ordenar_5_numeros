c6d52f8d28f1ab548ff284e31c01a81e7c98ff56
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-23 00:20:51 -0300

    primeiro commit ) dev

--- ordenar_5_numeros/Program.cs
+++ ordenar_5_numeros/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿int[] numeros = new int[5];
+int aux = 0;
+
+Console.WriteLine("Escreva 5 números inteiros");
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Escreva o {i+1}º número");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0;i < 5; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        if (numeros[j] > numeros[j + 1])
+        {
+            aux = numeros[j + 1];
+            numeros[j + 1] = numeros[j];
+            numeros[j] = aux;
+        }
+    }
+}
+Console.WriteLine("Números Ordenados:");
+for (int i = 0; i < 5 ; i++)
+{
+    Console.WriteLine(numeros[i]);
+}
