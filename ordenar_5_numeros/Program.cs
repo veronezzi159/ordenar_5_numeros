bf80c910f6f49322355c06e1f5bca4bc596c71cf
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-24 00:07:14 -0300

    commit inverso/ não repetir

--- ordenar_5_numeros/Program.cs
+++ ordenar_5_numeros/Program.cs
@@ -1,30 +1,76 @@
-﻿int[] numeros = new int[5], ordenado = new int[5];
-int aux = 0, menor = 0, j = 0, maior = 0;
+﻿using System.ComponentModel;
+int size = 150;
+int[] numeros = new int[size], ordenado = new int[size], inverso = new int[size] ;
+int aux = 0, menor = 0, comparacao = 0;
+bool imprime = true;
 
 Console.WriteLine("");
-for (int i = 0; i < 5; i++)
+for (int i = 0; i < size; i++)
 {
     numeros[i] = new Random().Next(0,100);
     ordenado[i] = numeros[i];
+    Console.Write(numeros[i] + " ");
 }
+Console.ReadLine();
 
-
-for(int i = 0; i < 5; i++)
+for(int referencia = 0; referencia < size; referencia++)
 {
-    for (j = i + 1; j < 5; j++)
+    for (comparacao = referencia + 1; comparacao < (size); comparacao++)
     {
-        if (ordenado[i] > ordenado[j])
+  
+        if (ordenado[referencia] > ordenado[comparacao])
         {
-            aux = ordenado[j];
-            ordenado[j] = ordenado[i];
-            ordenado[i] = aux;
+            aux = ordenado[referencia];
+            ordenado[referencia] = ordenado[comparacao];
+            ordenado[comparacao] = aux;
 
         }
     }
 }
 
+
 Console.WriteLine("Numeros ordenados:");
-for (int i = 0; i < 5 ; i++)
+
+
+for  (int referencia = 0;referencia < size ; referencia++)
 {
-    Console.WriteLine(ordenado[i]);
+    imprime = true;
+    for (comparacao = referencia + 1; comparacao < size; comparacao++)
+    {
+        if (ordenado[referencia] == ordenado[comparacao])
+        {
+            comparacao = size;
+            imprime = false;
+        }
+    }
+    if (imprime == true)
+    {
+        Console.Write(ordenado[referencia] + " ");
+    }
+}
+
+Console.WriteLine("Vetor invertido");
+for (int i = 0; i < size; i++)
+{
+    inverso[i] = ordenado[i];
+}
+
+for (int referencia = 0; referencia < size; referencia++)
+{
+    for (comparacao = referencia + 1; comparacao < (size); comparacao++)
+    {
+
+        if (inverso[referencia] < inverso[comparacao])
+        {
+            aux = inverso[referencia];
+            inverso[referencia] = inverso[comparacao];
+            inverso[comparacao] = aux;
+
+        }
+    }
+}
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write(inverso[i] + " ");
 }
